749d208c6d94b806abf8ba184970b7f0911e7b25
zahidmiran <zahidmiron599@gmail.com>
2022-05-22 17:02:42 +0600

    jwt genereted when a user login

--- index.js
+++ index.js
@@ -15,11 +15,46 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uhmti.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+/* Verify JWT */
+function verifyJWT(req, res, next) {
+    const authHeaders = req.headers.authorization;
+    if(!authHeaders){
+        return res.status(401).send({message: 'UnAuthorized Access' })
+    }
+    const token = authHeaders.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_KEY, function(err, decoded){
+        if(err){
+            return res.status(403).send({message:'Forbidden Access'})
+        }
+        decoded = req.decoded;
+        console.log(decoded, 'from jwt');
+        next();
+
+    })
+}
+
 
 async function run() {
     try {
       await client.connect();
       const partsCollection = client.db("manufactureable_parts").collection('allparts');
+      const userCollection = client.db("manufactureable_parts").collection('allUsers');
+      /* put Method When user login */
+      app.put('/user/:email', async(req,res)=>{
+          const email = req.params.email;
+          const user = req.body;
+          const filter = {email:email};
+          const option = {upsert: true};
+          const updateDoc = {
+              $set: user
+          };
+          const result = await userCollection.updateOne(filter, updateDoc, option);
+          const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_KEY, {expiresIn: '2h' } )
+          console.log(token);
+          res.status(200).send({token, result});
+
+
+      })
 
       /* add New Parts */
       app.post('/addParts', async(req, res)=>{
@@ -27,6 +62,11 @@ async function run() {
           console.log(parts);
           const result = partsCollection.insertOne(parts);
           res.send(result);
+      })
+      /* get all parts */
+      app.get('/allParts', async(req,res)=>{
+          const parts = await partsCollection.find().toArray();
+          res.send(parts);
       })
 
 
